9ee26456aa00c92f51e6135bce6dce983a20c88f
Nisheed Jagadish <nisheed@ajency.in>
2013-02-04 20:33:12 +0530

    initialization project.
    Project files added.

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+/*Project Name: Sapient Nitro - Round One.
+ * Developer: Nisheed Jagadish
+ * Repository: https://github.com/nisheed2440/sapientnitro.git
+ * Framework: Twitter Bootstrap
+ * CSS Framework: Less
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+ 	<head>
+ 		<title>Distinguished Technologist Program</title>
+    	<!-- Bootstrap -->
+    	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    	<!-- Less Css -->
+    	<link rel="stylesheet/less" type="text/css" href="css/main.less" />
+ 	</head>
+ 	<body>
+ 		<div class="container-fluid">
+ 			<header>
+ 				<div class="container sn-fixed-middle">
+    				<div class="sn-main-logo">
+    				</div>
+  				</div>
+  				<div class="clearfix"></div>
+ 			</header>
+ 			<div class="container sn-fixed-middle">
+ 				 <div class="row-fluid">
+				    <div class="span4">
+				      <!--Sidebar content-->
+				    </div>
+				    <div class="span8">
+				      <!--Body content-->
+				    </div>
+				 </div>
+ 			</div>
+ 		</div>
+ 		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.js"></script>
+ 		<script src="js/bootstrap.min.js"></script>
+ 		<script type="text/javascript" src="js/less.min.js"></script>
+ 	</body>
+</html>
